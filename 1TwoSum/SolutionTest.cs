5f37b2de462a8122982c07616c769b1143c5f3cf
Krzysztof Żmijewski <kzmijewski@protonmail.com>
2023-05-19 19:43:54 +0200

    - added: solution for 2AddTwoNumbers with XUnit test

--- 1TwoSum/SolutionTest.cs
+++ 1TwoSum/SolutionTest.cs
@@ -15,7 +15,7 @@ namespace _1TwoSum
         [InlineData(new[] { 2, 7, 11, 15 }, 9, new[] { 0, 1 })]
         [InlineData(new[] { 3, 2, 4 }, 6, new[] { 1, 2 })]
         [InlineData(new[] { 3, 3 }, 6, new[] { 0, 1 })]
-        [InlineData(new[] { 1, 2, 3, 4, 5 }, 10, new[] { 3, 4 })]
+        [InlineData(new[] { 1, 2, 3, 4, 5 }, 9, new[] { 3, 4 })]
         public void TestTwoSum(
             int[] nums, int target, int[] expected)
         {
