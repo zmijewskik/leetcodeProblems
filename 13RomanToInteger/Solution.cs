fb9bdc9ff0e524c7ec0fbcefe5c7cacb9d42834c
Krzysztof Żmijewski <kzmijewski@protonmail.com>
2023-05-20 21:00:11 +0200

    - rewritten few things for better understanding and memorizing

--- 13RomanToInteger/Solution.cs
+++ 13RomanToInteger/Solution.cs
@@ -30,5 +30,20 @@
             }
             return 0;
         }
+
+        public int RomanNumerals2(char c)
+        {
+            switch (c)
+            {
+                case 'I': return 1;
+                case 'V': return 5;
+                case 'X': return 10;
+                case 'L': return 50;
+                case 'C': return 100;
+                case 'D': return 500;
+                case 'M': return 1000;
+            }
+            return 0;
+        }
     }
 }
--- 1TwoSum/Solution.cs
+++ 1TwoSum/Solution.cs
@@ -11,4 +11,16 @@
             return new int[] { };
         }
     }
+
+    public class Solution2
+    {
+        public int[] TwoSum2(int[] nums, int target)
+        {
+            for (int i = 0; i < nums.Length; i++)
+                for (int j = 1 + 1; j < nums.Length; j++)
+                    if (nums[i] + nums[j] == target)
+                        return new int[] { i, j };
+            return new int[] { };
+        }
+    }
 }
