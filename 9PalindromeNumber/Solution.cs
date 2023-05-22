19c9c251d7e12cc4042da37a4ecb9fc9c631f177
Krzysztof Żmijewski <kzmijewski@protonmail.com>
2023-05-23 01:44:52 +0200

    Added: 2 new solutions, string>char -> reverse and LINQ reverse

--- 9PalindromeNumber/Solution.cs
+++ 9PalindromeNumber/Solution.cs
@@ -1,28 +1,41 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace _9PalindromeNumber
+﻿namespace _9PalindromeNumber
 {
     public class Solution
     {
+        //public bool IsPalindrome(int x)
+        //{
+        //    if (x < 0 || (x % 10 == 0 && x != 0))
+        //    {
+        //        return false;
+        //    }
+
+        //    int revertedNumber = 0;
+        //    while (x > revertedNumber)
+        //    {
+        //        revertedNumber = revertedNumber * 10 + x % 10;
+        //        x /= 10;
+        //    }
+
+        //    return x == revertedNumber || x == revertedNumber / 10;
+        //}
+
+        // solution with converting to string > char and reversing
+
+        //public bool IsPalindrome(int x)
+        //{
+        //    var y = x.ToString().ToCharArray();
+        //    Array.Reverse(y);
+        //    return x.ToString() == new string(y);
+        //}
+
+        // solution with converting to string and reversing using LINQ
+
         public bool IsPalindrome(int x)
         {
-            if (x < 0 || (x % 10 == 0 && x != 0))
-            {
+            var reverseString = x.ToString().Reverse();
+            if (string.Concat(reverseString) != x.ToString())
                 return false;
-            }
-
-            int revertedNumber = 0;
-            while (x > revertedNumber)
-            {
-                revertedNumber = revertedNumber * 10 + x % 10;
-                x /= 10;
-            }
-
-            return x == revertedNumber || x == revertedNumber / 10;
+            return true;
         }
     }
 }
--- 9PalindromeNumber/SolutionTest.cs
+++ 9PalindromeNumber/SolutionTest.cs
@@ -14,7 +14,7 @@ namespace _9PalindromeNumber
         [InlineData(1)]
         [InlineData(121)]
         [InlineData(9889)]
-        public void PalindromeNumber_ShouldReturnTrueWhenNumberIsAPalindrome(int input)
+        public void IsPalindrome_ShouldReturnTrueWhenNumberIsAPalindrome(int input)
         {
             // Arrange
             var solution = new Solution();
@@ -29,7 +29,7 @@ namespace _9PalindromeNumber
         [Theory]
         [InlineData(1234)]
         [InlineData(-121)]
-        public void PalindromeNumber_ShouldReturnFalseWhenNumberIsNotAPalindrome(int input)
+        public void IsPalindrome_ShouldReturnFalseWhenNumberIsNotAPalindrome(int input)
         {
             // Arrange
             var solution = new Solution();
@@ -41,56 +41,4 @@ namespace _9PalindromeNumber
             Assert.False(result);
         }
     }
-
-    //public class SolutionTests
-    //{
-    //    [Theory]
-    //    [InlineData(new[] { 2, 4, 3 }, new[] { 5, 6, 4 }, new[] { 7, 0, 8 })]
-    //    [InlineData(new[] { 0 }, new[] { 0 }, new[] { 0 })]
-    //    [InlineData(new[] { 9, 9, 9 }, new[] { 1 }, new[] { 0, 0, 0, 1 })]
-    //    public void AddTwoNumbers_ShouldReturnExpectedResult(int[] input1, int[] input2, int[] expected)
-    //    {
-    //        Arrange
-    //       var l1 = CreateLinkedList(input1);
-    //        var l2 = CreateLinkedList(input2);
-    //        var expectedList = CreateLinkedList(expected);
-
-    //        var solution = new Solution();
-
-    //        Act
-    //       var result = solution.AddTwoNumbers(l1, l2);
-
-    //        Assert
-    //        AssertLinkedListEquals(expectedList, result);
-    //    }
-
-    //    private ListNode CreateLinkedList(int[] values)
-    //    {
-    //        if (values == null || values.Length == 0) return null;
-
-    //        ListNode head = new ListNode(values[0]);
-    //        ListNode current = head;
-
-    //        for (int i = 1; i < values.Length; i++)
-    //        {
-    //            current.next = new ListNode(values[i]);
-    //            current = current.next;
-    //        }
-
-    //        return head;
-    //    }
-
-    //    private void AssertLinkedListEquals(ListNode expected, ListNode actual)
-    //    {
-    //        while (expected != null && actual != null)
-    //        {
-    //            Assert.Equal(expected.val, actual.val);
-    //            expected = expected.next;
-    //            actual = actual.next;
-    //        }
-
-    //        Assert.Null(expected);
-    //        Assert.Null(actual);
-    //    }
-    //}
 }
