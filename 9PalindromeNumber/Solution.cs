d36dd99dd0061e28f39213dafdf3d94ca4b3f970
Krzysztof Żmijewski <kzmijewski@protonmail.com>
2023-05-22 23:23:41 +0200

    -added solution to 9

---
+++ 9PalindromeNumber/Solution.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _9PalindromeNumber
+{
+    public class Solution
+    {
+        public bool IsPalindrome(int x)
+        {
+            if (x < 0 || (x % 10 == 0 && x != 0))
+            {
+                return false;
+            }
+
+            int revertedNumber = 0;
+            while (x > revertedNumber)
+            {
+                revertedNumber = revertedNumber * 10 + x % 10;
+                x /= 10;
+            }
+
+            return x == revertedNumber || x == revertedNumber / 10;
+        }
+    }
+}
