f877e53beacc0ffb2ff9307c482993bed408e9df
Krzysztof Żmijewski <kzmijewski@protonmail.com>
2023-05-23 11:26:59 +0200

    Added: 703 with solution and test

---
+++ 703KthLargestElementInAStream/KthLargestTest.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace _703KthLargestElementInAStream
+{
+    public class KthLargestTest
+    {
+        [Fact]
+        public void TestKthLargest()
+        {
+            // Arrange
+            int[] nums = new int[] { 4, 5, 8, 2 };
+            KthLargest kthLargest = new KthLargest(3, nums);
+
+            // Act and Assert
+            Assert.Equal(4, kthLargest.Add(3)); // return 4
+            Assert.Equal(5, kthLargest.Add(5)); // return 5
+            Assert.Equal(5, kthLargest.Add(10)); // return 5
+            Assert.Equal(8, kthLargest.Add(9)); // return 8
+            Assert.Equal(8, kthLargest.Add(4)); // return 8
+        }
+    }
+}
---
+++ 703KthLargestElementInAStream/Program.cs
@@ -0,0 +1,33 @@
+﻿Console.WriteLine("703KthLargestElementInAStream!");
+
+//int[] nums = new int[] { 4, 5, 8, 2 };
+//KthLargest kthLargest = new KthLargest(3, nums);
+//kthLargest.Add(3);
+//kthLargest.Add(5);
+//kthLargest.Add(10);
+//kthLargest.Add(9);
+//kthLargest.Add(8);
+
+public class KthLargest
+{
+    PriorityQueue<int, int> pq = new PriorityQueue<int, int>();
+    private static int kth = 1;
+    public KthLargest(int k, int[] nums)
+    {
+        kth = k;
+        foreach (var v in nums)
+        {
+            Add(v);
+        }
+    }
+
+    public int Add(int val)
+    {
+        pq.Enqueue(val, val);
+        if (pq.Count > kth)
+        {
+            pq.Dequeue();
+        }
+        return pq.Peek();
+    }
+}
