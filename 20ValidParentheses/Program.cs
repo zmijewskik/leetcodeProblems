92318d6f6b723d49ce452c426d6bfc83447b09cd
Krzysztof Żmijewski <kzmijewski@protonmail.com>
2023-05-25 20:23:24 +0200

    Added: 20 with 2 solutions and test

---
+++ 20ValidParentheses/Program.cs
@@ -0,0 +1,32 @@
+﻿Console.WriteLine("20ValidParentheses!");
+
+// using Stack
+public class ValidParentheses
+{
+    public bool IsValid(string s)
+    {
+        var k = new Stack<char>();
+        foreach (char c in s)
+        {
+            if (c == '(') { k.Push(')'); continue; }
+            if (c == '{') { k.Push('}'); continue; }
+            if (c == '[') { k.Push(']'); continue; }
+            if (k.Count == 0 || c != k.Pop()) return false;
+        }
+        return k.Count == 0;
+    }
+}
+
+// using .Replace
+//public class ValidParentheses
+//{
+//    public bool IsValid(string s)
+//    {
+//        while (s.Contains("()") || s.Contains("[]") || s.Contains("{}"))
+//        {
+//            s = s.Replace("()", "").Replace("[]", "").Replace("{}", "");
+//        }
+
+//        return s.Length == 0;
+//    }
+//}
---
+++ 20ValidParentheses/ValidParenthesesTest.cs
@@ -0,0 +1,23 @@
+﻿using Xunit;
+
+namespace _20ValidParentheses
+{
+    public class ValidParenthesesTest
+    {
+        [Theory]
+        [InlineData("()", true)]
+        [InlineData("()[]{}", true)]
+        [InlineData("(]", false)]
+        public void TestValidParentheses(string input, bool expected)
+        {
+            // Arrange
+            ValidParentheses solution = new ValidParentheses();
+
+            // Act
+            bool result = solution.IsValid(input);
+
+            // Assert
+            Assert.Equal(expected, result);
+        }
+    }
+}
