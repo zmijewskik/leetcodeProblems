717eac79c4043c25162ad1069534bdb6a2d5a67a
Krzysztof Żmijewski <kzmijewski@protonmail.com>
2023-05-26 23:37:28 +0200

    Added: Solution

---
+++ 27RemoveElement/SolutionTest.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+
+namespace _27RemoveElement
+{
+    public class SolutionTest
+    {
+        [Theory]
+        [InlineData(, true)]
+        [InlineData("()[]{}", true)]
+        public void RemoveElement_ReturnsNumberOfNotRemovedElements(
+            int[] input, int val, int expected)
+        {
+            // Arrange
+            ValidParentheses solution = new ValidParentheses();
+
+            // Act
+            bool result = solution.IsValid(input);
+
+            // Assert
+            Assert.Equal(expected, result);
+        }
+    }
+}
