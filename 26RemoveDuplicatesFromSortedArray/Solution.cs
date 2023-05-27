60aff557fc21b73a20d81b73621f81223d61d964
Krzysztof Żmijewski <kzmijewski@protonmail.com>
2023-05-28 00:11:28 +0200

    Added: solution with XUnit test

--- 26RemoveDuplicatesFromSortedArray/Solution.cs
+++ 26RemoveDuplicatesFromSortedArray/Solution.cs
@@ -6,7 +6,20 @@ using System.Threading.Tasks;
 
 namespace _26RemoveDuplicatesFromSortedArray
 {
-    internal class Solution
+    public class Solution
     {
+        public int RemoveDuplicates(int[] nums)
+        {
+            int insertIndex = 1;
+            for (int i = 1; i < nums.Length; i++)
+            {
+                if (nums[i - 1] != nums[i])
+                {
+                    nums[insertIndex] = nums[i];
+                    insertIndex++;
+                }
+            }
+            return insertIndex;
+        }
     }
 }
--- 26RemoveDuplicatesFromSortedArray/SolutionTest.cs
+++ 26RemoveDuplicatesFromSortedArray/SolutionTest.cs
@@ -3,10 +3,32 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Xunit;
 
 namespace _26RemoveDuplicatesFromSortedArray
 {
-    internal class SolutionTest
+    public class SolutionTest
     {
+        private readonly Solution _solution;
+
+        public SolutionTest()
+        {
+            _solution = new Solution();
+        }
+
+        [Theory]
+        [InlineData(new int[] { 1, 1, 2 }, 2, new int[] { 1, 2 })]
+        [InlineData(new int[] { 0, 0, 1, 1, 1, 2, 2, 3, 3, 4 }, 5, new int[] { 0, 1, 2, 3, 4 })]
+        [InlineData(new int[] { 1, 2, 3, 4, 5 }, 5, new int[] { 1, 2, 3, 4, 5 })]
+        public void TestRemoveDuplicates(int[] nums, int expectedLength, int[] expectedArray)
+        {
+            // Act
+            int actualLength = _solution.RemoveDuplicates(nums);
+            int[] actualArray = nums[..actualLength];
+
+            // Assert
+            Assert.Equal(expectedLength, actualLength);
+            Assert.Equal(expectedArray, actualArray);
+        }
     }
 }
