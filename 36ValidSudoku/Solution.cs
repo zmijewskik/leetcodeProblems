1ffb8ab70dcfb0010c853eccbb9cb4d9a177afcc
Krzysztof Żmijewski <kzmijewski@protonmail.com>
2023-05-28 00:40:48 +0200

    Added: 36 with solution

---
+++ 36ValidSudoku/Solution.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _36ValidSudoku
+{
+    public class Solution
+    {
+        public bool IsValidSudoku(char[][] board)
+        {
+            HashSet<char>[] row = new HashSet<char>[9];
+            HashSet<char>[] col = new HashSet<char>[9];
+            HashSet<char>[] box = new HashSet<char>[9];
+            for (int i = 0; i < 9; i++)
+            {
+                row[i] = new HashSet<char>();
+                col[i] = new HashSet<char>();
+                box[i] = new HashSet<char>();
+            }
+
+            for (int r = 0; r < board.Length; r++)
+            {
+                for (int c = 0; c < board[r].Length; c++)
+                {
+                    char elem = board[r][c];
+                    if (elem == '.')
+                    {
+                        continue;
+                    }
+
+                    if (!row[r].Add(elem))
+                    {
+                        return false;
+                    }
+
+                    if (!col[c].Add(elem))
+                    {
+                        return false;
+                    }
+
+                    int b = (3 * (r / 3)) + (c / 3);
+                    if (!box[b].Add(elem))
+                    {
+                        return false;
+                    }
+                }
+            }
+            return true;
+        }
+    }
+}
