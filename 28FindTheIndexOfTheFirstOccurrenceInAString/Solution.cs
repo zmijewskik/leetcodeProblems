8b4a51ae99a3bccac82625f11e36bff81075e249
Krzysztof Żmijewski <kzmijewski@protonmail.com>
2023-05-29 23:59:53 +0200

    Added: 28 solution

---
+++ 28FindTheIndexOfTheFirstOccurrenceInAString/Solution.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _28FindTheIndexOfTheFirstOccurrenceInAString
+{
+    public class Solution
+    {
+        public int StrStr(string haystack, string needle)
+        {
+            int m = needle.Length;
+            int n = haystack.Length;
+
+            for (int windowStart = 0; windowStart <= n - m; windowStart++)
+            {
+                for (int i = 0; i < m; i++)
+                {
+                    if (needle[i] != haystack[windowStart + i])
+                    {
+                        break;
+                    }
+                    if (i == m - 1)
+                    {
+                        return windowStart;
+                    }
+                }
+            }
+
+            return -1;
+        }
+    }
+}
