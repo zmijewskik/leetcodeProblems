0f002f0486bd73a5a6e0a533aefc341a8ab4dcf1
Krzysztof Żmijewski <kzmijewski@protonmail.com>
2023-06-02 22:19:54 +0200

    Added: 66 with solution and XUnit test

---
+++ 66PlusOne/Solution.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _66PlusOne
+{
+    public class Solution
+    {
+        public int[] PlusOne(int[] digits)
+        {
+            for (int i = digits.Length - 1; i >= 0; i--)
+            {
+                if (digits[i] == 9)
+                {
+                    digits[i] = 0;
+                }
+                else
+                {
+                    digits[i]++;
+                    return digits;
+                }
+            }
+            int[] firstOne = new int[] { 1 };
+            return firstOne.Concat(digits).ToArray();
+        }
+    }
+}
