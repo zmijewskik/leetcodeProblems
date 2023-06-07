5d00ecf085a2f88bd377fb4ece7e9260c263867d
Krzysztof Żmijewski <kzmijewski@protonmail.com>
2023-06-07 23:24:16 +0200

    Added: 1822 with solution and test

---
+++ 1822SignOfTheProductOfAnArray/Solution.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1822SignOfTheProductOfAnArray
+{
+    public class Solution
+    {
+        public int ArraySign(int[] nums)
+        {
+            int countNegativeNumbers = 0;
+            foreach (int number in nums)
+            {
+                // Edge case x*0 always = 0
+                if (number == 0) return 0;
+                if (number < 0) countNegativeNumbers++;
+            }
+            return countNegativeNumbers % 2 == 0 ? 1 : -1;
+        }
+    }
+}
---
+++ 1822SignOfTheProductOfAnArray/SolutionTest.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace _1822SignOfTheProductOfAnArray
+{
+    public class SolutionTest
+    {
+        private readonly Solution _solution;
+        public SolutionTest()
+        {
+            _solution = new Solution();
+        }
+
+        [Theory]
+        [InlineData(new int[] { -1, -2, -3, -4, 3, 2, 1 }, 1)]
+        [InlineData(new int[] { 1, 5, 0, 2, -3 }, 0)]
+        [InlineData(new int[] { -1, 1, -1, 1, -1 }, -1)]
+        public void TestArraySign(
+            int[] array, int expected)
+        {
+            // Act
+            var result = _solution.ArraySign(array);
+            // Assert
+            Assert.Equal(result, expected);
+        }
+    }
+}
