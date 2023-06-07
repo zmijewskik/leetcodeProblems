094d35ae1594ce16c7b5a50a2f556c6de589f636
Krzysztof Żmijewski <kzmijewski@protonmail.com>
2023-06-08 01:48:52 +0200

    Added: 121 with solution and test

---
+++ 121BestTimeToBuyAndSellStock/Solution.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _121BestTimeToBuyAndSellStock
+{
+    public class Solution
+    {
+        public int MaxProfit(int[] prices)
+        {
+            int minPrice = prices[0];
+            int maxProfit = 0;
+            for (int i = 1; i < prices.Length; i++)
+            {
+                if (prices[i] < minPrice)
+                    minPrice = prices[i];
+                else if (prices[i] - minPrice > maxProfit)
+                    maxProfit = prices[i] - minPrice;
+            }
+            return maxProfit;
+        }
+    }
+}
