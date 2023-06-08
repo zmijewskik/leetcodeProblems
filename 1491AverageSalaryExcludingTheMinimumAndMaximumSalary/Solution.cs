5f05b2dd2ebb1bd051bfb123d9b692f0693a38fe
Krzysztof Żmijewski <kzmijewski@protonmail.com>
2023-06-08 02:03:24 +0200

    Added: 1491 with solution and test

---
+++ 1491AverageSalaryExcludingTheMinimumAndMaximumSalary/Solution.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1491AverageSalaryExcludingTheMinimumAndMaximumSalary
+{
+    public class Solution
+    {
+        public double Average(int[] salaries)
+        {
+            int minSalary = int.MaxValue;
+            int maxSalary = int.MinValue;
+            int sum = 0;
+
+            foreach (int salary in salaries)
+            {
+                sum += salary;
+                minSalary = Math.Min(minSalary, salary);
+                maxSalary = Math.Max(maxSalary, salary);
+            }
+            return (double)(sum - minSalary - maxSalary) / (double)(salaries.Length - 2);
+        }
+    }
+}
---
+++ 1491AverageSalaryExcludingTheMinimumAndMaximumSalary/SolutionTest.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace _1491AverageSalaryExcludingTheMinimumAndMaximumSalary
+{
+    public class SolutionTest
+    {
+        private readonly Solution _solution;
+
+        public SolutionTest()
+        {
+            _solution = new Solution();
+        }
+
+        [Theory]
+        [InlineData(new int[] { 4000, 3000, 1000, 2000 }, 2500)]
+        [InlineData(new int[] { 1000, 2000, 3000 }, 2000)]
+        public void TestAverageSalary(
+            int[] salaries, int averageSalary)
+        {
+            // Assert
+            Assert.Equal(averageSalary, _solution.Average(salaries));
+        }
+    }
+}
