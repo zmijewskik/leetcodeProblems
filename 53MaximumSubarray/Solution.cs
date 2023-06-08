c29185674d03150f74384595a55480a27ba4d261
Krzysztof Żmijewski <kzmijewski@protonmail.com>
2023-06-08 22:52:05 +0200

    Added: 53 with solution and test

---
+++ 53MaximumSubarray/Solution.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _53MaximumSubarray
+{
+    public class Solution
+    {
+        public int MaxSubArray(int[] nums)
+        {
+            int sum = 0;
+            int maxSum = nums[0];
+
+            for (int i = 0; i < nums.Length; i++)
+            {
+                sum += nums[i];
+                if (nums[i] > sum)
+                {
+                    sum = nums[i];
+                }
+                if (sum > maxSum)
+                {
+                    maxSum = sum;
+                }
+            }
+            return maxSum;
+        }
+    }
+}
