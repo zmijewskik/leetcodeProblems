4c75d89808f1f92422f8756c42f768b8e49a2940
Krzysztof Żmijewski <kzmijewski@protonmail.com>
2023-06-08 21:20:50 +0200

    Added: 3 with solution and test

---
+++ 3LongestSubstringWithoutRepeatingCharacters/SolutionTest.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace _3LongestSubstringWithoutRepeatingCharacters
+{
+    public class SolutionTest
+    {
+        private readonly Solution _solution;
+
+        public SolutionTest()
+        {
+            _solution = new Solution();
+        }
+
+        [Theory]
+        [InlineData("abcabcbb", 3)]
+        [InlineData("bbbbb", 1)]
+        [InlineData("pwwkew", 3)]
+        public void TestLengthOfLongestSubstring(
+            string input, int count)
+        {
+            // Act
+            var result = _solution.LengthOfLongestSubstring(input);
+
+            // Assert
+            Assert.Equal(result, count);
+        }
+    }
+}
