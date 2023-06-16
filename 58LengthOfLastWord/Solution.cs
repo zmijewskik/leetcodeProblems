cf42717deeb7c3deb14dde990e3b2c92d4e6d7b9
Krzysztof Żmijewski <kzmijewski@protonmail.com>
2023-06-16 11:43:01 +0200

    Added: 58, 67 solutions

---
+++ 58LengthOfLastWord/Solution.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _58LengthOfLastWord
+{
+    public class Solution
+    {
+        public int LengthOfLastWord(string s)
+        {
+            var lastWordLetterCounter = 0;
+            for (int i = s.Length - 1; i >= 0;)
+            {
+                if (s[i--] != ' ') lastWordLetterCounter++;
+                else if (lastWordLetterCounter > 0) return lastWordLetterCounter;
+            }
+            return lastWordLetterCounter;
+        }
+    }
+}
---
+++ 67AddBinary/Solution.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _67AddBinary
+{
+    public class Solution
+    {
+        public string AddBinary(string a, string b)
+        {
+            var resultBuilder = new StringBuilder();
+            int remainder = 0;
+            for (int ai = a.Length - 1, bi = b.Length - 1; (ai >= 0 || bi >= 0);)
+            {
+                int charA = ai < 0 ? 0 : (int)Char.GetNumericValue(a[ai--]);
+                int charB = bi < 0 ? 0 : (int)Char.GetNumericValue(b[bi--]);
+
+                int sum = charA + charB + remainder;
+                remainder = 0;
+
+                resultBuilder.Insert(0, sum % 2);
+                if (sum >= 2) remainder = 1;
+            }
+            if (remainder == 1) resultBuilder.Insert(0, '1');
+            return resultBuilder.ToString();
+        }
+    }
+}
