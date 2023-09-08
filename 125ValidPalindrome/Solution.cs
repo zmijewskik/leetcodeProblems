4c20e67c9456e4fdf1c60294fd8b7cb982532c18
Krzysztof Żmijewski <kzmijewski@protonmail.com>
2023-09-08 23:40:57 +0200

    Added: 125

---
+++ 125ValidPalindrome/Solution.cs
@@ -0,0 +1,11 @@
+﻿namespace _125ValidPalindrome
+{
+    public class Solution
+    {
+        public bool IsPalindrome(string s)
+        {
+            var clean = s.ToLower().Where(x => char.IsLetterOrDigit(x));
+            return clean.Reverse().SequenceEqual(clean);
+        }
+    }
+}
---
+++ 125ValidPalindrome/SolutionTest.cs
@@ -0,0 +1,26 @@
+﻿using Xunit;
+
+namespace _125ValidPalindrome
+{
+    public class SolutionTest
+    {
+        private readonly Solution _solution;
+        public SolutionTest()
+        {
+            _solution = new Solution();
+        }
+
+        [Theory]
+        [InlineData("A man, a plan, a canal: Panama", true)]
+        [InlineData("race a car", false)]
+        [InlineData(" ", true)]
+        public void TestContainsDuplicates(string s, bool expected)
+        {
+            // Act
+            bool result = _solution.IsPalindrome(s);
+
+            // Assert
+            Assert.Equal(result, expected);
+        }
+    }
+}
