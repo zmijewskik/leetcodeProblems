1e830fca1c2e9421ab49b993c6a23bf062b37e39
Krzysztof Żmijewski <kzmijewski@protonmail.com>
2023-09-11 13:58:50 +0200

    Added: 118

--- 112PathSum/Solution.cs
+++ 112PathSum/Solution.cs
@@ -1,4 +1,25 @@
-﻿namespace _112PathSum
+﻿//Example 1:
+
+//Input: root = [5, 4, 8, 11, null, 13, 4, 7, 2, null, null, null, 1], targetSum = 22
+//Output: true
+//Explanation: The root-to - leaf path with the target sum is shown.
+
+//Example 2:
+
+//Input: root = [1, 2, 3], targetSum = 5
+//Output: false
+//Explanation: There two root - to - leaf paths in the tree:
+//(1-- > 2): The sum is 3.
+//(1 --> 3): The sum is 4.
+//There is no root-to-leaf path with sum = 5.
+
+//Example 3:
+
+//Input: root = [], targetSum = 0
+//Output: false
+//Explanation: Since the tree is empty, there are no root-to-leaf paths.
+
+namespace _112PathSum
 {
     // Definition for a binary tree node.
     public class TreeNode
---
+++ 118PascalsTriangle/Solution.cs
@@ -0,0 +1,40 @@
+﻿//Example 1:
+
+//Input: numRows = 5
+//Output: [[1],[1,1],[1,2,1],[1,3,3,1],[1,4,6,4,1]]
+
+//Example 2:
+
+//Input: numRows = 1
+//Output: [[1]]
+
+namespace _118PascalsTriangle
+{
+    // Dynamic Programming approach
+    public class Solution
+    {
+        public IList<IList<int>> Generate(int numRows)
+        {
+            List<IList<int>> triangle = new List<IList<int>>();
+            if (numRows == 0) return triangle;
+
+            triangle.Add(new List<int>() { 1 });
+
+            for (int i = 1; i < numRows; i++)
+            {
+                List<int> prevRow = (List<int>)triangle[i - 1];
+                List<int> newRow = new List<int> { 1 };
+
+                for (int j = 1; j < prevRow.Count; j++)
+                {
+                    newRow.Add(prevRow[j - 1] + prevRow[j]);
+                }
+
+                newRow.Add(1);
+                triangle.Add(newRow);
+            }
+
+            return triangle;
+        }
+    }
+}
