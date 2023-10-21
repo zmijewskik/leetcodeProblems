2225f982f4eb836119fecddd37d6675f25b761db
Krzysztof Żmijewski <kzmijewski@protonmail.com>
2023-10-21 22:00:38 +0200

    Added: 6

---
+++ 6ZigzagConversion/Solution.cs
@@ -0,0 +1,33 @@
+﻿using System.Text;
+
+namespace _6ZigzagConversion
+{
+    public class Solution
+    {
+        public string Convert(string s, int numRows)
+        {
+            if (numRows == 1)
+            {
+                return s;
+            }
+
+            var output = new StringBuilder(s.Length);
+            var period = numRows * 2 - 2;
+
+            for (int row = 0; row < numRows; ++row)
+            {
+                var increment = 2 * row;
+                for (int i = row; i < s.Length; i += increment)
+                {
+                    output.Append(s[i]);
+                    if (increment != period)
+                    {
+                        increment = period - increment;
+                    }
+                }
+            }
+
+            return output.ToString();
+        }
+    }
+}
