f8d74fdbe5ffadea4eaee81ed1daa8993ce2a029
Krzysztof Żmijewski <kzmijewski@protonmail.com>
2024-01-23 16:07:38 +0100

    Add: 1239

---
+++ 1239.Maximum Length of a Concatenated String with Unique Characters.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+public class Solution
+{
+    public int MaxLength(IList<string> arr, int i = 0, string s = "")
+    {
+
+        if (s.Distinct().Count() != s.Length) return 0;
+
+        if (arr.Count == i) return s.Length;
+
+        return Math.Max(
+            MaxLength(arr, i + 1, s),
+            MaxLength(arr, i + 1, s + arr[i])
+        );
+    }
+}
