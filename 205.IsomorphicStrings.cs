cb254af984db00a1c8bcf0fffcba93d709662b3f
Krzysztof Żmijewski <kzmijewski@protonmail.com>
2024-01-27 17:56:55 +0100

    Add: 205

---
+++ 205.IsomorphicStrings.cs
@@ -0,0 +1,14 @@
+﻿public class Solution
+{
+    public bool IsIsomorphic(string s, string t)
+    {
+        for (int i = 0; i < s.Length; i++)
+        {
+            if (s.IndexOf(s[i], i + 1) != t.IndexOf(t[i], i + 1))
+            {
+                return false;
+            }
+        }
+        return true;
+    }
+}
