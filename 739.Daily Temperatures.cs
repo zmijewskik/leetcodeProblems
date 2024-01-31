e1907db61970c72c605fbac24c17c767ba0c9992
Krzysztof Żmijewski <kzmijewski@protonmail.com>
2024-01-31 17:33:41 +0100

    Add: 739

---
+++ 739.Daily Temperatures.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Generic;
+
+public class Solution
+{
+    public int[] DailyTemperatures(int[] temperatures)
+    {
+        int[] results = new int[temperatures.Length];
+        Stack<int> stack = new Stack<int>();
+
+        for (int i = 0; i < temperatures.Length; i++)
+        {
+            while (stack.Count > 0 && temperatures[stack.Peek()] < temperatures[i])
+            {
+                int index = stack.Pop();
+                results[index] = i - index;
+            }
+            stack.Push(i);
+        }
+        return results;
+    }
+}
