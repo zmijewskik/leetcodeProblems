48ac007a72b977066b8001e350987b2402fb5e51
Krzysztof Żmijewski <kzmijewski@protonmail.com>
2024-02-03 23:57:42 +0100

    Add: 226

---
+++ 226.Invert Binary Tree.cs
@@ -0,0 +1,29 @@
+﻿/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     public int val;
+ *     public TreeNode left;
+ *     public TreeNode right;
+ *     public TreeNode(int val=0, TreeNode left=null, TreeNode right=null) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+public class Solution
+{
+    public TreeNode InvertTree(TreeNode root)
+    {
+        if (root == null)
+        {
+            return null;
+        }
+
+        TreeNode temp = root.left;
+        root.left = InvertTree(root.right);
+        root.right = InvertTree(temp);
+
+        return root;
+    }
+}
