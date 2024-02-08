6f8996877e0388df0ea8a2153c99d9edf6889ca5
Krzysztof Żmijewski <kzmijewski@protonmail.com>
2024-02-08 23:15:05 +0100

    Add: 234

---
+++ 234.Palindrome Linked List.cs
@@ -0,0 +1,47 @@
+﻿/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     public int val;
+ *     public ListNode next;
+ *     public ListNode(int val=0, ListNode next=null) {
+ *         this.val = val;
+ *         this.next = next;
+ *     }
+ * }
+ */
+public class Solution
+{
+    public bool IsPalindrome(ListNode head)
+    {
+        var slow = head;
+        var fast = head;
+        ListNode prev = null;
+
+        while (fast != null && fast.next != null)
+        {
+            fast = fast.next.next;
+            ListNode next = slow.next;
+            slow.next = prev;
+            prev = slow;
+            slow = next;
+        }
+
+        if (fast != null)
+        {
+            slow = slow.next;
+        }
+
+        while (slow != null)
+        {
+            if (slow.val != prev.val)
+            {
+                return false;
+            }
+
+            slow = slow.next;
+            prev = prev.next;
+        }
+
+        return true;
+    }
+}
