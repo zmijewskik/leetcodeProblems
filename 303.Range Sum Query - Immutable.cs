7a22fdfb174ce8b765f7ff7f95ef8b4076a69270
Krzysztof Żmijewski <kzmijewski@protonmail.com>
2024-02-13 16:44:20 +0100

    Add: 303

---
+++ 303.Range Sum Query - Immutable.cs
@@ -0,0 +1,23 @@
+﻿public class NumArray
+{
+
+    private int[] _nums;
+
+    public NumArray(int[] nums)
+    {
+        var n = nums.Length;
+        _nums = new int[n + 1];
+        for (int i = 0; i < n; i++) _nums[i + 1] = _nums[i] + nums[i];
+    }
+
+    public int SumRange(int left, int right)
+    {
+        return _nums[right + 1] - _nums[left];
+    }
+}
+
+/**
+ * Your NumArray object will be instantiated and called as such:
+ * NumArray obj = new NumArray(nums);
+ * int param_1 = obj.SumRange(left,right);
+ */
