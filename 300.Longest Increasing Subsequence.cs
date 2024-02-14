a599eed540bf5fa98af00298219c54383496d93e
Krzysztof Żmijewski <kzmijewski@protonmail.com>
2024-02-14 23:36:45 +0100

    Add: 300

---
+++ 300.Longest Increasing Subsequence.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+public class Solution
+{
+    public int LengthOfLIS(int[] nums)
+    {
+        int c = 0;
+        for (int i = 1; i < nums.Length; i++)
+        {
+            if (nums[i] > nums[c])
+            {
+                nums[++c] = nums[i];
+                continue;
+            }
+
+            var idx = Array.BinarySearch(nums, 0, c, nums[i]);
+            if (idx < 0)
+            {
+                nums[~idx] = nums[i];
+            }
+        }
+        return c + 1;
+    }
+}
