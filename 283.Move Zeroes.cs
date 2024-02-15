5ac68f2f8835ecfdc3999cbdf5b1811ea0bffaff
Krzysztof Żmijewski <kzmijewski@protonmail.com>
2024-02-15 23:35:27 +0100

    Add: 283

---
+++ 283.Move Zeroes.cs
@@ -0,0 +1,16 @@
+﻿public class Solution
+{
+    public void MoveZeroes(int[] nums)
+    {
+        int left = 0;
+
+        for (int right = 0; right < nums.Length; right++)
+        {
+            if (nums[right] != 0)
+            {
+                (nums[left], nums[right]) = (nums[right], nums[left]);
+                left++;
+            }
+        }
+    }
+}
