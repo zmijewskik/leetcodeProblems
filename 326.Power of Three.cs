73dc86bbbefa11402efad46e805a1920e50ef60e
Krzysztof Żmijewski <kzmijewski@protonmail.com>
2024-02-19 14:46:18 +0100

    Add: 326

---
+++ 326.Power of Three.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+public class Solution
+{
+    public bool IsPowerOfThree(int n)
+    {
+        // int32 max 3 Pow is 19
+        var maxPow = (int)Math.Pow(3, 19);
+
+        return n > 0 && maxPow % n == 0;
+    }
+}
