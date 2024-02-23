47238db0ecbc94eed9dd52c803b8b374c508504f
Krzysztof Żmijewski <kzmijewski@protonmail.com>
2024-02-23 14:55:44 +0100

    Add: 374, 375

---
+++ 374.Guess Number Higher or Lower.cs
@@ -0,0 +1,47 @@
+﻿/** 
+ * Forward declaration of guess API.
+ * @param  num   your guess
+ * @return 	     -1 if num is higher than the picked number
+ *			      1 if num is lower than the picked number
+ *               otherwise return 0
+ * int guess(int num);
+ */
+
+public class Solution : GuessGame
+{
+    public int GuessNumber(int n)
+    {
+        int low = 1;
+        int high = n;
+
+        while (low <= high)
+        {
+            // Find the middle point
+            int mid = low + (high - low) / 2;
+
+            // Call the pre-defined API to get feedback
+            int result = guess(mid);
+
+            // Check if we have found the answer
+            if (result == 0)
+            {
+                return mid;
+            }
+
+            // Check if we need to search in the left half
+            else if (result == -1)
+            {
+                high = mid - 1;
+            }
+
+            // Check if we need to search in the right half
+            else
+            {
+                low = mid + 1;
+            }
+        }
+
+        // Return -1 if no answer is found
+        return -1;
+    }
+}
---
+++ 375.Guess Number Higher or Lower II.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+public class Solution
+{
+    public int GetMoneyAmount(int n)
+    {
+        // 2D array stores computed results for subproblems
+        int[,] table = new int[n + 1, n + 1];
+
+        return DP(table, 1, n);
+    }
+
+    int DP(int[,] t, int s, int e)
+    {
+        // base case
+        if (s >= e) return 0;
+        if (t[s, e] != 0) return t[s, e];
+
+        int res = int.MaxValue;
+
+        for (int x = s; x <= e; x++)
+        {
+            int tmp = x + Math.Max(DP(t, s, x - 1), DP(t, x + 1, e));
+            res = Math.Min(res, tmp);
+        }
+        // memoization of DP 
+        t[s, e] = res;
+
+        return res;
+    }
+}
