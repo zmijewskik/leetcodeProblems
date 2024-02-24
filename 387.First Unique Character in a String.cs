86b23aa57ef51ed9c30b3ca9a355c40159b0aa86
Krzysztof Żmijewski <kzmijewski@protonmail.com>
2024-02-25 00:19:17 +0100

    Add: 387

---
+++ 387.First Unique Character in a String.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections.Generic;
+
+public class Solution
+{
+    public int FirstUniqChar(string s)
+    {
+        int n = s.Length;
+        if (n == 0)
+        {
+            return -1;
+        }
+
+        Dictionary<char, int> charCount = new Dictionary<char, int>();
+
+        foreach (char ch in s)
+        {
+            if (charCount.ContainsKey(ch))
+            {
+                charCount[ch]++;
+            }
+            else
+            {
+                charCount[ch] = 1;
+            }
+        }
+
+        for (int i = 0; i < n; i++)
+        {
+            if (charCount[s[i]] == 1)
+            {
+                return i;
+            }
+        }
+
+        return -1;
+    }
+}
