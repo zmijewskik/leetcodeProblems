aa13954722465510c04d203271ae3fa345d3ea09
Krzysztof Żmijewski <kzmijewski@protonmail.com>
2024-02-24 23:35:07 +0100

    Add: 383

---
+++ 383.Ransom Note.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections.Generic;
+
+public class Solution
+{
+    public bool CanConstruct(string ransomNote, string magazine)
+    {
+        Dictionary<char, int> letters = new Dictionary<char, int>();
+        foreach (char letter in magazine)
+        {
+            if (letters.ContainsKey(letter))
+            {
+                letters[letter] += 1;
+            }
+            else
+            {
+                letters[letter] = 1;
+            }
+        }
+        foreach (char c in ransomNote)
+        {
+            if (letters.ContainsKey(c) && letters[c] > 0)
+            {
+                letters[c] -= 1;
+            }
+            else
+            {
+                return false;
+            }
+        }
+        return true;
+    }
+}
