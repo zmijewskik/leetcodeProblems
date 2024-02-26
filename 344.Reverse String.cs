f85fc0171f06da3eacb43ec95196262dff69bf1d
Krzysztof Żmijewski <kzmijewski@protonmail.com>
2024-02-27 00:31:06 +0100

    Add: 344

---
+++ 344.Reverse String.cs
@@ -0,0 +1,14 @@
+﻿public class Solution
+{
+    public void ReverseString(char[] s)
+    {
+        int i = 0;
+        int j = s.Length - 1;
+        while (i < j)
+        {
+            char temp = s[i];
+            s[i++] = s[j];
+            s[j--] = temp;
+        }
+    }
+}
